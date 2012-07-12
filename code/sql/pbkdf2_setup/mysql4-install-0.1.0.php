704a43c9d18bc39cea5b60db3467fd4ca46c4980
Vinai Kopp <vinai@netzarbeiter.com>
2012-07-12 09:39:50 +0200

    Implement copy of existing password hashes to new value table

--- code/sql/pbkdf2_setup/mysql4-install-0.1.0.php
+++ code/sql/pbkdf2_setup/mysql4-install-0.1.0.php
@@ -8,8 +8,29 @@ $installer->getConnection()->modifyColumn(
     $installer->getTable('admin/user'), 'password', 'VARCHAR(800) default NULL'
 );
 
-$installer->updateAttribute('customer', 'password_hash', 'backend_type', 'text');
+$newAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'password_hash');
+$currentAttribute = clone $newAttribute;
+if ('text' !== $currentAttribute->getBackendType()) {
+    $newAttribute = clone $currentAttribute;
+    $newAttribute->setBackendType('text');
 
-// TODO copy all passwords to the eav_entity_text table and encrypt the hashes with PBKDF2
+    $currentTable = $currentAttribute->getBackend()->getTable();
+    $newTable = $newAttribute->getBackend()->getTable();
+
+    // Copy password hashes over to the text attribute value table
+    $cols = array('entity_type_id', 'attribute_id', 'entity_id', 'value');
+    $sql = $installer->getConnection()->select()
+            ->from($currentTable, $cols)
+            ->where('attribute_id=?', $currentAttribute->getId())
+            ->insertFromSelect($newTable, $cols);
+    $installer->getConnection()->query($sql);
+
+    $installer->updateAttribute('customer', 'password_hash', 'backend_type', 'text');
+
+    // Delete values from old value table
+    $installer->getConnection()->delete($currentTable, array('attribute_id=?' => $currentAttribute->getId()));
+}
+
+// TODO encrypt the hashes with PBKDF2
 
 $installer->endSetup();
