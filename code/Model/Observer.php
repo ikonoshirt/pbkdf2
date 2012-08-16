8cd2103d8b4c1e216e30e15a404c827d9de4625d
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-08-16 13:35:27 +0200

    changed the event to hook into to core_collection_abstract_load_before, because controller_action_predispatch is not thrown for Mage::app() changed method names to fit Zend coding standard

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -1,13 +1,40 @@
 <?php
+/**
+ * In this file you find all the observer methods used in the pbkdf2 module
+ *
+ * PHP Version 5.2.13
+ *
+ * @category Magento
+ * @package  Ikonoshirt_Pbkdf2
+ * @author   Fabian Blechschmidt <fabian.blechschmidt@ikonoshirt.de>
+ * @license  http://www.ikonoshirt.de/stuff/licenses/beerware-fabian.txt THE BEER-WARE LICENSE
+ * @version  GIT: <git_id>
+ * @link     https://github.com/ikonoshirt/pbkdf2
+ * @php
+ */
+
+/**
+ * Observer for PBKDF2 Module
+ *
+ * @category Magento
+ * @package  Ikonoshirt_Pbkdf2
+ * @author   Fabian Blechschmidt <fabian.blechschmidt@ikonoshirt.de>
+ * @license  http://www.ikonoshirt.de/stuff/licenses/beerware-fabian.txt THE BEER-WARE LICENSE
+ * @link     https://github.com/ikonoshirt/pbkdf2
+ */
 class Ikonoshirt_Pbkdf2_Model_Observer
 {
     /**
-     * If the customer's password is an old MD5 hash, and the shop-owner wants them to replaced, DO IT.
+     * If the customer's password is an old MD5 hash, and the shop-owner
+     * wants them to replaced, DO IT.
      *
-     * @param $observer
+     * @param Mage_Core_Model_Observer $observer Observer with customer informations
+     *
+     * @return void
      */
-    public function customer_customer_authenticated($observer) {
-        if(!(boolean)Mage::getStoreConfig('ikonoshirt/pbkdf2/check_legacy_hash')) {
+    public function customerCustomerAuthenticated($observer)
+    {
+        if (!(boolean)Mage::getStoreConfig('ikonoshirt/pbkdf2/check_legacy_hash')) {
             return;
         }
 
@@ -22,7 +49,10 @@ class Ikonoshirt_Pbkdf2_Model_Observer
         $encrypter = $helper->getEncryptor();
 
         // if the hash validates against the old hashing method, replace the hash
-        if($encrypter->validateLegacyHash($password, $customer->getPasswordHash())) {
+        if ($encrypter->validateLegacyHash(
+            $password, $customer->getPasswordHash()
+        )
+        ) {
             $customer->setPassword($password);
             $customer->save();
         }
@@ -31,10 +61,13 @@ class Ikonoshirt_Pbkdf2_Model_Observer
     /**
      * If the api password is hashed the old way, we replace it
      *
-     * @param $observer
+     * @param Mage_Core_Model_Observer $observer Observer with API user informations
+     *
+     * @return void
      */
-    public function api_user_authenticated($observer) {
-        if(!(boolean)Mage::getStoreConfig('ikonoshirt/pbkdf2/check_legacy_hash')) {
+    public function apiUserAuthenticated($observer)
+    {
+        if (!(boolean)Mage::getStoreConfig('ikonoshirt/pbkdf2/check_legacy_hash')) {
             return;
         }
 
@@ -46,18 +79,23 @@ class Ikonoshirt_Pbkdf2_Model_Observer
         $user = $observer->getModel();
         $password = $observer->getApiKey();
         Mage::log('test');
-        if($encrypter->validateLegacyHash($password, $user->getApiKey())) {
+        if ($encrypter->validateLegacyHash($password, $user->getApiKey())) {
             $user->setApiKey($observer->getApiKey());
             $user->save();
         }
     }
 
     /**
-     * If the admin's password is an old MD5 hash, and the shop-owner wants them to replaced, DO IT.
+     * If the admin's password is an old MD5 hash, and the shop-owner wants
+     * them to replaced, DO IT.
      *
-     * @param $observer
+     * @param Mage_Core_Model_Observer $observer observer with information about
+     *                                           admin user
+     *
+     * @return void
      */
-    public function admin_user_authenticate_after($observer) {
+    public function adminUserAuthenticateAfter($observer)
+    {
         /* @var $helper Mage_Core_Helper_Data */
         $helper = Mage::helper('core');
         $encrypter = $helper->getEncryptor();
@@ -65,17 +103,35 @@ class Ikonoshirt_Pbkdf2_Model_Observer
         /* @var $user Mage_Admin_Model_User */
         $user = $observer->getUser();
         $password = $observer->getPassword();
-        if($encrypter->validateLegacyHash($password, $user->getPassword())) {
+        if ($encrypter->validateLegacyHash($password, $user->getPassword())) {
             $user->setPassword($observer->getPassword());
         }
     }
 
     /**
-     * If the magento version is Enterprise Edition we have to change the Stub we extend from
+     * If the magento version is Enterprise Edition we have to change the Stub we
+     * extend from
+     *
+     * @param Mage_Core_Model_Observer $observer Observer with event informations
+     *
+     * @return void
      */
-    public function controller_action_predispatch() {
+    public function coreCollectionAbstractLoadBefore($observer)
+    {
+        $isStoreCollection = $observer->getCollection() instanceof
+        Mage_Core_Model_Resource_Store_Collection;
+        if (!$isStoreCollection) {
+            // we only want to hook up the first call of this event. The first call
+            // is for the store collection
+            return;
+        }
+        // Mage_Core_Model_Resource_Store_Collection
+
         if ((string)Mage::getConfig()->getNode('modules/Enterprise_Pci/active')) {
-            Mage::getConfig()->setNode('global/helpers/core/encryption_model', 'Ikonoshirt_Pbkdf2_Model_Stub_EE');
+            Mage::getConfig()->setNode(
+                'global/helpers/core/encryption_model',
+                'Ikonoshirt_Pbkdf2_Model_Stub_EE'
+            );
 
         }
     }
