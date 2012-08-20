cc4afe9c0ffb19323b8c0cf66106ae9f60ae939e
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-08-20 21:52:14 +0200

    ... more stubs, more tests

---
+++ code/Test/Model/Encryption.php
@@ -0,0 +1,32 @@
+<?php
+
+class Ikonoshirt_Pbkdf2_Test_Model_Encryption extends EcomDev_PHPUnit_Test_Case
+{
+    public function testValidateHashWithPrefix()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testValidateHashWithoutPrefix()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testValidateHashWithSalt()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testValidateHashWithMd5()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
