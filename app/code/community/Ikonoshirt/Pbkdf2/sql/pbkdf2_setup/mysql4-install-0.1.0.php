958efd8ae05a6b6dd23f1cb1149a38879010fb80
Colin Mollenhour <colin@mollenhour.com>
2014-10-23 15:18:12 -0400

    Use transaction in setup script. Use better getRandomString for salt.

--- app/code/community/Ikonoshirt/Pbkdf2/sql/pbkdf2_setup/mysql4-install-0.1.0.php
+++ app/code/community/Ikonoshirt/Pbkdf2/sql/pbkdf2_setup/mysql4-install-0.1.0.php
@@ -12,28 +12,36 @@ $newAttribute = Mage::getSingleton('eav/config')
     ->getAttribute('customer', 'password_hash');
 $currentAttribute = clone $newAttribute;
 if ('text' !== $currentAttribute->getBackendType()) {
-    $newAttribute = clone $currentAttribute;
-    $newAttribute->setBackendType('text');
 
-    $currentTable = $currentAttribute->getBackend()->getTable();
-    $newTable = $newAttribute->getBackend()->getTable();
+    $installer->getConnection()->beginTransaction();
+    try {
+        $newAttribute = clone $currentAttribute;
+        $newAttribute->setBackendType('text');
 
-    // Copy password hashes over to the text attribute value table
-    $cols = array('entity_type_id', 'attribute_id', 'entity_id', 'value');
-    $sql = $installer->getConnection()->select()
-        ->from($currentTable, $cols)
-        ->where('attribute_id=?', $currentAttribute->getId())
-        ->insertFromSelect($newTable, $cols);
-    $installer->getConnection()->query($sql);
+        $currentTable = $currentAttribute->getBackend()->getTable();
+        $newTable = $newAttribute->getBackend()->getTable();
 
-    $installer->updateAttribute(
-        'customer', 'password_hash', 'backend_type', 'text'
-    );
+        // Copy password hashes over to the text attribute value table
+        $cols = array('entity_type_id', 'attribute_id', 'entity_id', 'value');
+        $sql = $installer->getConnection()->select()
+            ->from($currentTable, $cols)
+            ->where('attribute_id=?', $currentAttribute->getId())
+            ->insertFromSelect($newTable, $cols);
+        $installer->getConnection()->query($sql);
 
-    // Delete values from old value table
-    $installer->getConnection()->delete(
-        $currentTable, array('attribute_id=?' => $currentAttribute->getId())
-    );
+        $installer->updateAttribute(
+            'customer', 'password_hash', 'backend_type', 'text'
+        );
+
+        // Delete values from old value table
+        $installer->getConnection()->delete(
+            $currentTable, array('attribute_id=?' => $currentAttribute->getId())
+        );
+        $installer->getConnection()->commit();
+    } catch (Exception $e) {
+        $installer->getConnection()->rollBack();
+        throw $e;
+    }
 }
 
 $installer->endSetup();
